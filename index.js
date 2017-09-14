a0fc5b6b14c930590fb3b802337f146d4dd98734
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-14 12:27:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for(var i = 0; i < musicians.length; i++) {
+<<<<<<< HEAD
     array.push(`${musicians[i]} plays ${instruments[i]}`);
+=======
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+>>>>>>> 52173f2c58266d0f1a544ac7ff927ec515ccc5c0
   }
   return array;
 }
 
+<<<<<<< HEAD
 function johnLennonFacts(array) {
   var newArray = [];
   var i = 0;
@@ -22,4 +27,16 @@ function iLoveTheBeatles(n) {
     newArray.push("I love the Beatles!");
   } while(n < 15 && newArray.length <= n);
   return newArray;
+=======
+function johnLennonFacts(facts) {
+
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!") * n;
+  } while (n < 15);
+  return array;
+>>>>>>> 52173f2c58266d0f1a544ac7ff927ec515ccc5c0
 }
